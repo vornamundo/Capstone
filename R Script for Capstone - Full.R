10648e1d095d57d2dc4664a0bfaf0d845e56877a
vornamundo <annaleosol@googlemail.com>
2018-01-20 11:25:25 +0000

    Full R script for Capstone
    Removed troublesome '#try importing file...' chunk as it wasn't needed

--- R Script for Capstone - Full.R
+++ R Script for Capstone - Full.R
@@ -16,8 +16,6 @@ allfilenames <- substr(allfiles,1,33)
 allcsvfilenames <- allfilenames [1:285]
 #try out reading a single csv file
 testimport <- read_csv("UCASData/EOC_data_resource_2016-DR2_001_01.csv", col_names = TRUE, skip = 5)
-#try importing file and creating filename for 1 file
-names <-substr(filenames,1,33)EOC_data_resource_2016_DR3_027_03 <- read_csv("UCASData/EOC_data_resource_2016-DR3_027_03.csv", col_names = TRUE, skip = 5)
 #after much trying out lapply and other functions, gave up and imported 3 files I knew were relevant
 EOC_data_resource_2016_DR3_027_03 <- read_csv("UCASData/EOC_data_resource_2016-DR3_027_03.csv", col_names = TRUE, skip = 5)
 View(EOC_data_resource_2016_DR3_027_03)  
